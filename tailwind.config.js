5439e590c781a12076cd39bf3a548ed7bfa79643
Elijan Mastnak <git@ejmastnak.com>
2025-12-18 18:35:12 +0100

    style: add on-theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,19 @@ export default {
   ],
   theme: {
     extend: {
+      colors: {
+        sc: {
+          orange: '#f7941d',
+          white: '#fef3e6',
+          pink: '#fea7c6',
+          brown: '#2d2821',
+          dark: '#26231e'
+        },
+      },
+      fontFamily: {
+        sans: ['Montserrat', ...defaultTheme.fontFamily.sans],
+      },
+
       fontFamily: {
         'sans': ['Montserrat', ...defaultTheme.fontFamily.sans],
       },
