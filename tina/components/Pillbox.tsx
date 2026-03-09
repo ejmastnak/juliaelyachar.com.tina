de7790fde78efd30eb82b87d17a85b404e7f874c
Elijan Mastnak <git@ejmastnak.com>
2026-03-09 08:05:47 +0100

    fix: correct className

--- tina/components/Pillbox.tsx
+++ tina/components/Pillbox.tsx
@@ -1,9 +1,9 @@
 type Props = {
-  classes: string;
+  className: string;
   children: ReactNode;
 };
 
-export default function Pillbox({classes, children}: Props) {
+export default function Pillbox({className, children}: Props) {
 
   return (
     <span className={`inline-flex items-center w-fit rounded-full bg-indigo-50 px-2 py-1 text-xs font-medium text-indigo-700 ring-1 ring-inset ring-indigo-700/10 text-nowrap ${className ?? ""}`}>{children}</span>
