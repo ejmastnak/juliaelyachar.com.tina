7d01a106de168fb4219cae252ccbc1f0dcf0a8f9
Elijan Mastnak <git@ejmastnak.com>
2026-03-28 07:03:16 +0100

    fix: add client-side sorting of events 2
    Was missing on Home page; see commit fd927d1

--- tina/pages/Index/partials/Events.tsx
+++ tina/pages/Index/partials/Events.tsx
@@ -1,4 +1,5 @@
 import { tinaField } from "tinacms/dist/react";
+import { useEffect, useState } from "react";
 import type { MyHomePageQuery, Event, } from "@tina/__generated__/types";
 import { TinaMarkdown } from "tinacms/dist/rich-text";
 import LinkButton from '@tina/components/LinkButton.tsx'
@@ -11,22 +12,55 @@ type Props = {
 
 export default function About({ homePage, events }: Props) {
 
-  const today = new Date()
-  const N = 2
+  const EVENTS_TO_DISPLAY = 2
 
-  const upcomingEvents = events.filter(e => new Date(e.date) >= today).sort((a, b) => a.date >= b.date ? 1 : -1);
-  const pastEvents = events.filter(e => new Date(e.date) < today).sort((a, b) => a.date < b.date ? 1 : -1);
+  function sortAsc(a, b) {
+    return new Date(a.date) - new Date(b.date);
+  }
+
+  function sortDesc(a, b) {
+    return new Date(b.date) - new Date(a.date);
+  }
+
+  function splitEvents(events, today) {
+    const upcoming = [];
+    const past = [];
+
+    events.forEach((e) => {
+      const date = new Date(e.date);
+      if (date >= today) upcoming.push(e);
+        else past.push(e);
+    });
+
+    return {
+      upcoming: upcoming.sort(sortAsc),
+      past: past.sort(sortDesc),
+    };
+  }
+
+  // SSG fallback (runs at build time) 
+  const initial = splitEvents(events, new Date());
+
+  // Runs during SSG, and also client-side if JS enabled
+  const [upcomingEvents, setUpcomingEvents] = useState(initial.upcoming);
+  const [pastEvents, setPastEvents] = useState(initial.past);
+
+  useEffect(() => {
+    const { upcoming, past } = splitEvents(events, new Date());
+    setUpcomingEvents(upcoming);
+    setPastEvents(past);
+  }, [events]);
 
   return (
     <div>
       <h2 data-tina-field={tinaField(homePage, "eventsHeading")} className="text-5xl">{homePage.eventsHeading}</h2>
       <ul role="list" className="mt-8 lg:mt-12 flex flex-col gap-y-8 w-fit">
-        {upcomingEvents.slice(0, N).map((event) => (
+        {upcomingEvents.slice(0, EVENTS_TO_DISPLAY).map((event) => (
           <li key={event.id}><Event event={event}/></li>
         ))}
         <li className="border-t border-gray-300"/>
-        {upcomingEvents.length < N && 
-          pastEvents.slice(0, N - pastEvents.length).map((event) => (
+        {upcomingEvents.length < EVENTS_TO_DISPLAY && 
+          pastEvents.slice(0, EVENTS_TO_DISPLAY - pastEvents.length).map((event) => (
             <li key={event.id}><Event event={event}/></li>
           ))
         }
